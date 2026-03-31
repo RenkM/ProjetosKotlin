cbcfa4d4ba0c419fe0b81b26e70299aba86450ce
Matheus Renk <renkzeme@gmail.com>
2026-03-31 12:15:46 -0300

    Update Carro.kt

--- corridaCarros/src/Carro.kt
+++ corridaCarros/src/Carro.kt
@@ -1,13 +1,13 @@
+// classe que representa um carro
 class Carro (
-    val modelo: String,
-    val velocidade: Int,
-    val aceleracao: Int,
-    val freio: Int,
-    val tanque: Int,
-    val consumo: Int,
-    val resistencia: Int,
-    val piloto: Piloto) //local em que defino os atributos
-{
 
+    val modelo: String,        // nome do carro
+    val velocidade: Int,       // velocidade máxima
+    val aceleracao: Int,       // rapidez pra ganhar velocidade
+    val freio: Int,            // eficiência de frenagem
+    val tanque: Int,           // capacidade de combustível
+    val consumo: Int,          // quanto consome por volta
+    val resistencia: Int,      // "vida" do carro
+    val piloto: Piloto         // piloto que dirige o carro
 
-}
+)
