6f1725e4e3baed1859c585122a83b73634461b2f
Matheus Renk <renkzeme@gmail.com>
2026-03-31 12:15:18 -0300

    Update Piloto.kt

--- corridaCarros/src/Piloto.kt
+++ corridaCarros/src/Piloto.kt
@@ -1,4 +1,8 @@
+// classe que representa um piloto (tipo um molde)
 class Piloto (
-    val nome: String,
-    val idade: Int,
-    val habilidade: Int)//local em defino as atributos
+
+    val nome: String,       // nome do piloto
+    val idade: Int,         // idade dele
+    val habilidade: Int     // nível de habilidade (quanto maior, melhor)
+
+)
